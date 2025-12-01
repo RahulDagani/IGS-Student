c97e532d1c6b8aea8d2ca2a157a2fc21863648ed
igsdeveloper01 <igsdeveloper01@gmail.com>
2025-12-01 13:12:21 +0530

    Remove Prisma

--- prisma/migrations/20251101102110_add_password_and_phone_number_fields/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `password` to the `apply_tenant_users` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "apply_tenant_users" ADD COLUMN     "password" TEXT NOT NULL,
-ADD COLUMN     "phone_number" TEXT;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,6 @@
 // middleware.ts
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { AuthService } from '@/lib/auth';
 
 // Define role-based route access
 const routePermissions = {

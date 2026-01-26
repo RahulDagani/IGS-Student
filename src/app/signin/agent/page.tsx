ad0c363cd9d8ea1a7c3df5bf0f6b166fb1ce77e4
ashokindoglobalstudies-sketch <ashok.indoglobalstudies@gmail.com>
2026-01-26 20:26:06 +0530

    agent login api route correction

--- src/app/signin/agent/page.tsx
+++ src/app/signin/agent/page.tsx
@@ -132,7 +132,7 @@ function AgentLoginContent() {
 
 
     try {
-      const response = await fetch(`${BASE_URL}/login`, {
+      const response = await fetch(`${BASE_URL}/agent/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",

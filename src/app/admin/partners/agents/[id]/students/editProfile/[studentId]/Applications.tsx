d6a12bcd86ebc5375933c89d5da82e55c5f600ce
igsdeveloper01 <igsdeveloper01@gmail.com>
2026-02-18 09:41:02 +0530

    fix add agent student

--- src/app/admin/partners/agents/[id]/students/editProfile/[studentId]/Applications.tsx
+++ src/app/admin/partners/agents/[id]/students/editProfile/[studentId]/Applications.tsx
@@ -1760,7 +1760,24 @@ const updateCredentials = async () => {
             </div>
           ) : (
             <div className="col-span-8 bg-white dark:bg-gray-800 border dark:border-gray-700 rounded-md p-6 flex items-center justify-center">
-              <div className="text-gray-600 dark:text-gray-400">Select an application to view details</div>
+              {applications.length > 0 
+              ? <div className="text-gray-600 dark:text-gray-400">Select an application to view details</div>
+              : <>
+              
+              <div className="text-gray-600 dark:text-gray-400 block">Applications Not found for this Student</div>
+              
+            <button
+            onClick={()=>setActiveTab("apply")} 
+            className="block h-11 px-4 rounded-lg border-2 border-green-500 bg-transparent text-sm text-green-500 shadow-theme-xs hover:bg-green-50 dark:hover:bg-green-900/20 flex items-center gap-2">
+              <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
+              </svg>
+              Create Application
+            </button>
+          
+              </>
+            }
+
             </div>
           )}
         </div>
